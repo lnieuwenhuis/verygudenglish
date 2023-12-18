5234b64bccc943fa2774f9981cd25365653c289a
Lars Nieuwenhuis <35393046+lnieuwenhuis@users.noreply.github.com>
2023-12-18 14:23:44 +0100

    additions

--- resources/views/studenten.blade.php
+++ resources/views/studenten.blade.php
@@ -7,14 +7,16 @@
 </head>
 
 <body>
-    <nav class="bg-blue-700 h-fit flex flex-row pb-1">
-        <div class="h-full p-3">Logo</div>
-        <div class="h-full p-3">Woordenschat-Game</div>
-        <div class="ml-auto p-3">[student naam]</div>
-        <div class="p-3">Logout</div>
-    </nav>
+
     <div>
-        <div class="bg-gray-400 h-2/3 w-3/4 m-auto mt-5 mb-5">
+        <nav class="bg-blue-700 h-fit flex flex-row pb-1">
+            <div class="h-full p-3">Logo</div>
+            <div class="h-full p-3">Woordenschat-Game</div>
+            <div class="ml-auto p-3">[student naam]</div>
+            <div class="p-3">Logout</div>
+        </nav>
+
+        <div class="bg-gray-400 h-2/3 w-3/4 m-auto mt-5 mb-5 rounded">
             <div class="flex flex-col">
                 {{-- Later Foreach periode toevoegen --}}
                 <div class="flex flex-row m-3">
@@ -44,7 +46,7 @@
 
             </div>
         </div>
-        <div class="bg-gray-400 h-2/3 w-3/4 m-auto mt-5 mb-5">
+        <div class="bg-gray-400 h-2/3 w-3/4 m-auto mt-5 mb-5 rounded">
             <div class="p-3">Woordenlijst</div>
         </div>
     </div>
