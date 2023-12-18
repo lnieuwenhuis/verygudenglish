66745f66aed4f9759d7303cd43770286965293ca
Lars Nieuwenhuis <35393046+lnieuwenhuis@users.noreply.github.com>
2023-12-18 11:45:24 +0100

    started studenten

--- resources/views/studenten.blade.php
+++ resources/views/studenten.blade.php
@@ -7,7 +7,10 @@
 </head>
 
 <body>
-    <nav class="h-6 bg-blue-600">
-
+    <nav class="bg-blue-700 h-fit flex flex-row pb-1">
+        <div class="h-full p-3">Logo</div>
+        <div class="h-full p-3">Woordenschat-Game</div>
+        <div class="ml-auto p-3">[student naam]</div>
+        <div class="p-3">Logout</div>
     </nav>
 </body>
