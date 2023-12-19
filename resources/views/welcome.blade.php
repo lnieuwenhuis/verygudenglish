cae6ecc911d9a717b4215cff3f15d89bb949850a
Lars Nieuwenhuis <35393046+lnieuwenhuis@users.noreply.github.com>
2023-12-19 22:07:57 +0100

    front-end

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,13 +11,14 @@
 </head>
 
 <body>
-    <nav class="bg-white border-gray-200 dark:bg-gray-800">
+    <nav class="bg-white border-gray-200 dark:bg-gray-400">
         <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
             <a href="localhost:8000" class="flex items-center space-x-3 rtl:space-x-reverse">
                 <img src="https://upload.wikimedia.org/wikipedia/commons/4/42/Flag_of_the_United_Kingdom.png"
                     class="h-8 w-14" alt="Engelse Vlag" />
-                <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Engels
-                    Woordenschat</span>
+                <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-gray-700">
+                    Engels Woordenschat
+                </span>
             </a>
             <button data-collapse-toggle="navbar-default" type="button"
                 class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
@@ -31,10 +32,10 @@
             </button>
             <div class="hidden w-full md:block md:w-auto" id="navbar-default">
                 <ul
-                    class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-800 dark:border-gray-700">
-                    <li>
+                    class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-500 md:dark:bg-gray-500 dark:border-gray-700">
+                    <li class="p-1">
                         <a href="#" {{-- TODO: Link naar Landstede Azure toevoegen --}}
-                            class="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
+                            class="block py-2 px-3 text-gray-700 bg-blue-700 rounded md:bg-transparent md:text-gray-700 md:p-0 dark:text-gray-700 md:dark:text-gray-700"
                             aria-current="page">Inloggen</a>
                     </li>
 
@@ -42,9 +43,10 @@
             </div>
         </div>
     </nav>
-    <div class="bg-gray-400 h-screen">
+    <div class="bg-gray-800 h-screen">
         <div class="w-3/4 ml-auto mr-auto bg-gray-500 h-full">
-            <div class="text-center text-2xl font-semibold pt-5">Leer hier eenvoudig jouw Engelse Woordenschat!
+            <div class="text-center text-2xl font-semibold pt-5 dark:text-gray-800">Leer hier eenvoudig jouw Engelse
+                Woordenschat!
             </div>
         </div>
     </div>
