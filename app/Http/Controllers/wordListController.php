5830a2063c0104d80d67d5dd02a304da52ec681e
313xx <116277233+313xx@users.noreply.github.com>
2023-12-19 09:22:52 +0100

    model

--- app/Http/Controllers/wordListController.php
+++ app/Http/Controllers/wordListController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class listController extends Controller
+class wordListController extends Controller
 {
     //
 }
--- app/Models/Words.php
+++ app/Models/Words.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class userController extends Model
+class Words extends Model
 {
     use HasFactory;
 }
