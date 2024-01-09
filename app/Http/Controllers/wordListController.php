a81128313966ae491d631f242198657440382b5b
Lars Nieuwenhuis <35393046+lnieuwenhuis@users.noreply.github.com>
2024-01-09 09:37:23 +0100

    wordlist controller edited

--- app/Http/Controllers/wordListController.php
+++ app/Http/Controllers/wordListController.php
@@ -2,33 +2,39 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\WordList;
 use Illuminate\Http\Request;
 
 class wordListController extends Controller
 {
-    public function index(){
-
-
+    public function index()
+    {
+        return view("wordList");
     }
-    public function create(){
-
-
+    public function create()
+    {
     }
-    public function store(){
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required|max:100',
+        ]);
 
+        $wordList = new WordList;
+        $wordList->title = $request->get('title');
+        $wordList->save();
 
+        $wordList->words()->attach($request->get('words'));
+
+        return redirect()->back()->with('message', 'Wordlist Stored in DB');
     }
-    public function edit(){
-
-
+    public function edit()
+    {
     }
-    public function update(){
-
-
+    public function update()
+    {
     }
-    public function delete(){
-
-
+    public function delete()
+    {
     }
-
 }
