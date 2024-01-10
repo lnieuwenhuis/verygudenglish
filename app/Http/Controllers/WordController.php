e5b0c2afb5fe795bf012ea3ba84feafd2547cd91
Lars Nieuwenhuis <35393046+lnieuwenhuis@users.noreply.github.com>
2024-01-10 13:08:32 +0100

    Update WordController.php

--- app/Http/Controllers/WordController.php
+++ app/Http/Controllers/WordController.php
@@ -2,33 +2,41 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Word;
 use Illuminate\Http\Request;
 
 class wordController extends Controller
 {
-    public function index(){
-
-
+    public function index()
+    {
+        return view('docenten.wordlists.words', ['words' => Word::all()]);
     }
-    public function create(){
-
-
+    public function create()
+    {
     }
-    public function store(){
+    public function store(Request $request)
+    {
+        $request->validate([
+            'word' => 'required',
+            'wordlist' => 'required'
+        ]);
 
+        $word = new Word;
+        $word->word = $request->get('word');
+        $word->wordlist = $request->get('wordlist');
+        $word->save();
 
+        $word->wordlist()->attach($request->get('wordlist'));
+
+        return redirect()->back()->with('message', 'Words Stored in Word List');
     }
-    public function edit(){
-
-
+    public function edit()
+    {
     }
-    public function update(){
-
-
+    public function update()
+    {
     }
-    public function delete(){
-
-
+    public function delete()
+    {
     }
-
 }
