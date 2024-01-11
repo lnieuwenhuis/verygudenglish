0891a72f6c578723f596de94e3af8b71737abbe8
Lars Nieuwenhuis <35393046+lnieuwenhuis@users.noreply.github.com>
2024-01-11 13:34:39 +0100

    periode seeder

---
+++ database/seeders/PeriodSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Period;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+class PeriodSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $periods = [
+            ['title' => '1', 'is_locked' => '1'],
+            ['title' => '2', 'is_locked' => '0'],
+            ['title' => '3', 'is_locked' => '0'],
+            ['title' => '4', 'is_locked' => '1']
+        ];
+
+        foreach ($periods as $period) {
+            Period::create($period);
+        }
+    }
+}
