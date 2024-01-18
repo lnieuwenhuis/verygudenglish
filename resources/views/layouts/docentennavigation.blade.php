75bf2898c330271f30ab945989b0bd9bfaed509c
ItsArrox <jesserowanhoekstra@outlook.com>
2024-01-18 15:26:16 +0100

    docenten routes added

--- resources/views/layouts/docentennavigation.blade.php
+++ resources/views/layouts/docentennavigation.blade.php
@@ -3,11 +3,7 @@
         <div class="sidebar-header flex items-center justify-center py-4">
             <div class="inline-flex">
                 <a href="#" class="inline-flex flex-row items-center">
-                    <svg class="w-10 h-10 text-red-400" fill="currentColor" viewBox="0 0 20 20">
-                        <path fill-rule="evenodd"
-                            d="M11.757 2.034a1 1 0 01.638.519c.483.967.844 1.554 1.207 2.03.368.482.756.876 1.348 1.467A6.985 6.985 0 0117 11a7.002 7.002 0 01-14 0c0-1.79.684-3.583 2.05-4.95a1 1 0 011.707.707c0 1.12.07 1.973.398 2.654.18.374.461.74.945 1.067.116-1.061.328-2.354.614-3.58.225-.966.505-1.93.839-2.734.167-.403.356-.785.57-1.116.208-.322.476-.649.822-.88a1 1 0 01.812-.134zm.364 13.087A2.998 2.998 0 017 13s.879.5 2.5.5c0-1 .5-4 1.25-4.5.5 1 .786 1.293 1.371 1.879.586.585.879 1.353.879 2.121s-.293 1.536-.879 2.121z"
-                            clip-rule="evenodd" />
-                    </svg>
+                    <link rel="icon" type="image/x-icon" href="https://imgur.com/dUJsQ8V.png">
                     <span class="leading-10 text-gray-100 text-2xl font-bold ml-1 uppercase">Very Gud English</span>
                 </a>
             </div>
@@ -25,7 +21,7 @@
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M15 19.128a9.38 9.38 0 0 0 2.625.372 9.337 9.337 0 0 0 4.121-.952 4.125 4.125 0 0 0-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 0 1 8.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0 1 11.964-3.07M12 6.375a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0Zm8.25 2.25a2.625 2.625 0 1 1-5.25 0 2.625 2.625 0 0 1 5.25 0Z" />
                             </svg>
                         </span>
-                        <span class="ml-3">Studenten</span>
+                        <span class="ml-3" href="{{ route('studenten.index') }}">Studenten</span>
                     </a>
                 </li>
                 <li class="my-px">
@@ -38,7 +34,7 @@
                                     d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01" />
                             </svg>
                         </span>
-                        <span class="ml-3">Toetsen</span>
+                        <span class="ml-3" href="{{ route('toetsen.index') }}">Toetsen</span>
                     </a>
                 </li>
                 <li class="my-px">
@@ -50,7 +46,7 @@
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 10.5H21A7.5 7.5 0 0 0 13.5 3v7.5Z" />
                             </svg>
                         </span>
-                        <span class="ml-3">Periodes</span>
+                        <span class="ml-3" href="{{ route('periodes.index') }}">Periodes</span>
                     </a>
                 </li>
                 <li class="my-px">
