d25f940d8bb798aed25502f0ebb3fd00e3b3cb33
Lars Nieuwenhuis <35393046+lnieuwenhuis@users.noreply.github.com>
2024-01-22 09:29:45 +0100

    Update studenten.blade.php

--- resources/views/docenten/studenten.blade.php
+++ resources/views/docenten/studenten.blade.php
@@ -12,27 +12,18 @@
 </head>
 
 <x-app-layout>
-    <div class="bg-gray-400 h-2/3 w-3/4 m-auto mt-5 mb-5 rounded flex flex-row justify-evenly">
-        <div class="p-3 text-lg">
-            <a href="{{ route('toetsen.index') }}">Toetsen</a>
+    <h1 class="text-2xl font-extrabold p-5">Studenten</h1>
+    <div class="flex flex-col pl-5">
+        <div class="flex flex-row border-b-gray-400 border-b-2">
+            <div class="">Naam</div>
+            <div class="ml-auto">Type</div>
         </div>
-        <div class="p-3 text-lg">
-            <a href="{{ route('periodes.index') }}">Periodes</a>
-        </div>
-        <div class="p-3 text-lg">
-            <a href="{{ route('docenten') }}">Home</a>
-        </div>
-        <div class="p-3 text-lg">
-            <a href="{{ route('studenten.index') }}">Studenten</a>
-        </div>
-        <div class="p-3 text-lg">
-            <a href="{{ route('woordenlijsten.index') }}">Woordenlijsten</a>
-        </div>
+
+        @foreach ($studenten as $student)
+            <div class="flex flex-row">
+                <div class="text-white m-1 p-1 bg-gray-600 rounded-lg">{{ $student['name'] }}</div>
+                <div class="text-white m-1 p-1 bg-gray-600 rounded-lg ml-auto">{{ $student['type'] }}</div>
+            </div>
+        @endforeach
     </div>
-    @foreach ($studenten as $student)
-        <div class="flex flex-row">
-            <div class="text-white m-2 p-1 bg-gray-700 rounded-lg">{{ $student['name'] }}</div>
-            <div class="text-white m-2 p-1 bg-gray-700 rounded-lg">{{ $student['type'] }}</div>
-        </div>
-    @endforeach
 </x-app-layout>
