36710e4a9a89a81a2681b8cdc396482e1a432b0b
Lars Nieuwenhuis <35393046+lnieuwenhuis@users.noreply.github.com>
2024-02-01 14:38:53 +0100

    kleine push hihi

---
+++ app/Http/Controllers/StudentController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\ProfileUpdateRequest;
+use App\Models\Period;
+use App\Models\Student;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Redirect;
+
+class StudentController extends Controller
+{
+    public function index()
+    {
+        return view('studenten');
+    }
+    public function studentPeriodes()
+    {
+        return view('studenten.periodes', ['periodes' => Period::all()]);
+    }
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required',
+            'password' => 'required',
+            'remember_token' => 'required',
+        ]);
+
+        $user = new Student;
+        $user->name = $request->get('name');
+        $user->email = $request->get('email');
+        $user->password = Hash::make($request->get('password'));
+        $user->remember_token = $request->get('remember_token');
+
+        $user->save();
+
+        return redirect()->back()->with('message', 'User Stored');
+    }
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required',
+            'password' => 'required',
+            'remember_token' => 'required',
+        ]);
+
+        $user = Student::findOrFail($id);
+        $user->name = $request->get('name');
+
+        $user->save();
+    }
+}
---
+++ database/seeders/DocentSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Docent;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class DocentSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+
+
+        $users = [
+            ['name' => Str::random('10'), 'email' => Str::random('10'), 'password' => Hash::make('password'), 'remember_token' => 'true'],
+            ['name' => Str::random('10'), 'email' => Str::random('10'), 'password' => Hash::make('password'), 'remember_token' => 'true'],
+            ['name' => Str::random('10'), 'email' => Str::random('10'), 'password' => Hash::make('password'), 'remember_token' => 'true'],
+            ['name' => Str::random('10'), 'email' => Str::random('10'), 'password' => Hash::make('password'), 'remember_token' => 'true'],
+            ['name' => Str::random('10'), 'email' => Str::random('10'), 'password' => Hash::make('password'), 'remember_token' => 'true'],
+        ];
+
+        foreach ($users as $user) {
+            Docent::create($user);
+        }
+    }
+}
