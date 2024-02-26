2aae535296c77617ec4153ae6bcaba88a661bf1d
Lars Nieuwenhuis <35393046+lnieuwenhuis@users.noreply.github.com>
2024-02-26 09:37:48 +0100

    Update AgeOfWordsController.php

--- app/Http/Controllers/AgeOfWordsController.php
+++ app/Http/Controllers/AgeOfWordsController.php
@@ -3,9 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Models\Period;
+use App\Models\Student;
 use App\Models\Word;
 use App\Models\WordList;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AgeOfWordsController extends Controller
 {
@@ -19,8 +21,9 @@ class AgeOfWordsController extends Controller
 
             $periodId = WordList::findOrFail($period_id);
             $listId = WordList::findOrFail($request->list_id);
+            $userId = Auth::id();
 
-            return view('games.ageofwords', ['words' => $words, 'period_id' => $periodId, 'list_id' => $listId]);
+            return view('games.ageofwords', ['words' => $words, 'period_id' => $periodId, 'list_id' => $listId, 'user_id' => $userId]);
         }
     }
 }
